82906c296eddd1f75f77235af76fb4660396aab4
Trong.Tran <trong.tran@sapotacorp.com>
2022-08-24 09:53:25 +0700

    init project

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const puppeteer = require("puppeteer");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.API_PORT || 3005;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("./file_cv"));
+
+app.get("/export-cv/:hash", async (req, res) => {
+  try {
+    const { hash } = req.params;
+    const fileName = `cv${new Date().toString().replace(/ /g, "_")}.pdf`;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://localhost:3000/generated/${hash}`);
+    await page.pdf({
+      path: `./file_cv/${fileName}`,
+      format: "A4",
+      printBackground: true,
+      preferCSSPageSize: true,
+    });
+    await page.close();
+    res
+      .status(200)
+      .json({ url: `https://localhost:${process.env.API_PORT}/${fileName}` });
+  } catch (err) {
+    res.status(500).json({ message: "somthing went wrong!" });
+  }
+});
+
+app.listen(port, () =>
+  console.log(`server run with https://localhost:${port}`)
+);
